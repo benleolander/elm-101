fd9c39925b30c8b73d80c68327292e7c9beea33e
Ben Lander <ben@benlander.me>
2020-10-09 14:12:05 +0000

    + Model

--- src/Main.elm
+++ src/Main.elm
@@ -3,3 +3,16 @@ module Main exposing (..)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+
+main = 
+	Browser.sandbox { init = init, update = update, view = view }
+	
+-- Model 
+type alias Model = 
+	Int
+	
+init : Model
+init = 
+	0
+	
+	
