3631ad5fb5655e874f14868cd46e09b248bdf172
Ben Lander <ben@benlander.me>
2020-10-09 14:35:17 +0000

    More tabs fixing, working

--- src/Main.elm
+++ src/Main.elm
@@ -6,26 +6,26 @@ import Html.Events exposing (onClick)
 
 main = 
     Browser.sandbox { init = init, update = update, view = view }
-	
+
 -- Model 
 type alias Model = 
     Int
-	
+
 init : Model
 init = 
     0
-	
+
 -- Update
 type Msg = Increment | Decrement
-	
+
 update : Msg -> Model -> Model
 update msg model = 
     case msg of 
         Increment ->
             model + 1
         Decrement ->
-        model - 1	
-	
+            model - 1
+
 -- View
 view : Model -> Html Msg
 view model = 
